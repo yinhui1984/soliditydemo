4b729007d205a32132b4d86ef0ee567701874a04
z <g44784803@gmail.com>
2024-01-05 09:10:56 +0800

    update

--- test/ecrecoverdemo.t.sol
+++ test/ecrecoverdemo.t.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.10;
 
 import "forge-std/Test.sol";
-import "src/ecrecoverDemo.sol";
+import "src/ecrecoverdemo.sol";
 
 contract EcrecoverDemoTest is Test {
     EcrecoverDemo ecd;
